e1232accae601738ea79492d7dbf51f77c50c751
Shariful Islam <ether_sb@yahoo.com>
2023-08-31 17:51:12 +0600

    sms panel half done

---
+++ database/migrations/2023_08_31_041357_create_sms_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmsLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms_logs', function (Blueprint $table) {
+            $table->id();
+            $table->text('contact')->nullable();
+            $table->string('messageID')->nullable();
+            $table->text('sms')->nullable()->collation('utf8mb4_unicode_ci');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sms_logs');
+    }
+}
